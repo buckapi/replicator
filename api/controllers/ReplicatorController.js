01f05a3e8df6d866015c0331656bc0e25a3de23e
Buckapi <112070278+buckapi@users.noreply.github.com>
2023-11-25 17:25:00 -0500

    Update ReplicatorController.js

--- api/controllers/ReplicatorController.js
+++ api/controllers/ReplicatorController.js
@@ -4,14 +4,18 @@
  * @description :: Server-side actions for handling incoming requests.
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
+
 const { exec } = require('child_process');
 
 module.exports = {
   runRsync: function(req, res) {
     let project_name = req.body.project_name;
     let source_dir = req.body.source_dir;
-    let dest_dir = req.body.dest_dir;
+    let base_dest_dir = req.body.dest_dir;
     let exclude_dir = req.body.exclude_dir;
+
+    // Construye la ruta del directorio destino incluyendo el nombre del proyecto
+    let dest_dir = `${base_dest_dir}/${project_name}`;
 
     let command = `rsync -avz --exclude=${exclude_dir} ${source_dir} ${dest_dir}`;
 
@@ -26,4 +30,3 @@ module.exports = {
     });
   }
 };
-
